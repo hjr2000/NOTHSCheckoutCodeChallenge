828476d81405a56145a22a0c6779356ec4cba492
hjr2000 <hjr2000@users.noreply.github.com>
2020-10-26 14:08:51 +0000

    Add in product Datatable functionality

--- src/test/Features/CheckoutTests.feature
+++ src/test/Features/CheckoutTests.feature
@@ -3,5 +3,15 @@ Feature: Tests to Exercise Checkout Functionality
   @Runme
   Scenario: test 1
     Given I am running the following promotions
-      | Travel card holder promo                  |
-      | Over 60 pounds spend 10 pc discount promo |
+      |   Travel card holder promo                  |
+      |   Over 60 pounds spend 10 pc discount promo |
+    When I scan the following products in the checkout
+      |   Travel Card Holder    |
+      |   Personalised Cufflinks|
+      |   Kids T-shirt          |
+
+#  @Runme
+#  Scenario: test 2
+#    Given I am running the following promotions
+#      | Travel card holder promo                  |
+#
--- src/test/java/com/checkout/StepDefs.java
+++ src/test/java/com/checkout/StepDefs.java
@@ -2,23 +2,49 @@ package com.checkout;
 
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.When;
 
 import java.util.List;
 
 public class StepDefs {
 
     Item[] items;
+    List<String> promotionsList;
+    List<String> productsList;
 
-//    @Given("I say hello")
-//    public void iSayHello() {
-//
-//        System.out.println("Hellooooooooooooooooooooooooooooooooooooo!");
-//    }
+
+    @Before
+    public void setUpDataItem() {
+
+        System.out.println("***** BEFORE ******");
+        items = new Item[]{
+                new Item("001", "Travel Card Holder", 9.25),
+                new Item("002", "Personalised Cufflinks", 9.25),
+                new Item("003", "Kids T-shirt", 9.25)
+        };
+    }
 
     @Given("I am running the following promotions")
-    public void iAmRunningTheFollowingPromotions(DataTable table) {
+    public void iAmRunningTheFollowingPromotions(DataTable promotionsDatatable) {
 
-       List<String> rows = table.asList(String.class);
-        System.out.println("Hellooooooooooooooooooooooooooooooooooooo!");
+        promotionsList = promotionsDatatable.asList(String.class);
+        System.out.println("-------------------------------------------");
+        for (String promotionName:promotionsList){
+            System.out.println(promotionName);
+        }
+        System.out.println("-------------------------------------------");
+    }
+
+    @When("I scan the following products in the checkout")
+    public void iScanTheFollowingProductsInTheCheckout(DataTable productsDatatable) {
+
+        productsList = productsDatatable.asList(String.class);
+        System.out.println("-------------------------------------------");
+        for (String productName:productsList){
+            System.out.println(productName);
+        }
+        System.out.println("-------------------------------------------");
+
     }
 }
